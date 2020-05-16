73e09755d0d28866a06ca8b50c118ebdeb9f778a
Oleksii Mykhailov <alem.coding@gmail.com>
2020-05-16 15:58:31 +0300

    Updates

---
+++ precache-manifest.79f30ae9705345089f333ee8b846df14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23b21443740b59efec3622fcd4e9bbd",
+    "url": "/react_datatable/index.html"
+  },
+  {
+    "revision": "29f10d5a03afb952ff61",
+    "url": "/react_datatable/static/css/main.c121ece8.chunk.css"
+  },
+  {
+    "revision": "ae3b29e7b854562a5baf",
+    "url": "/react_datatable/static/js/2.c1380065.chunk.js"
+  },
+  {
+    "revision": "29f10d5a03afb952ff61",
+    "url": "/react_datatable/static/js/main.33a1575d.chunk.js"
+  },
+  {
+    "revision": "cd26d19e335861885027",
+    "url": "/react_datatable/static/js/runtime~main.ce12b3cb.js"
+  }
+]);
